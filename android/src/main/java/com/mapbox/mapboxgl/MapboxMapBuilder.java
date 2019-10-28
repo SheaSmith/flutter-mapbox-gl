f1f4e5f93bfec1cc6a11be7de8c15571a5ed1e37
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2019-10-28 14:32:27 +1300

    Re-enable authentication

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapBuilder.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapBuilder.java
@@ -21,7 +21,7 @@ class MapboxMapBuilder implements MapboxMapOptionsSink {
   public final String TAG = getClass().getSimpleName();
   private final MapboxMapOptions options = new MapboxMapOptions()
     .textureMode(true)
-    .attributionEnabled(false);
+    .attributionEnabled(true);
   private boolean trackCameraPosition = false;
   private boolean myLocationEnabled = false;
   private int myLocationTrackingMode = 0;
